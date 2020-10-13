efac6be6c5229aa4bf5606bbbe8a32c6b929cafb
Vaskevich Aleksander <foltfrend@list.ru>
2020-10-13 11:45:20 +0300

    init support Feature Schema #19 (#24)

--- marshmallow_geojson/geometry_collection.py
+++ marshmallow_geojson/geometry_collection.py
@@ -12,22 +12,8 @@ from .object_type import (
 from ._base import BaseSchema
 from .geometry import GeometriesSchema
 
-from .point import PointSchema
-from .multi_polygon import MultiPolygonSchema
-from .line_string import LineStringSchema
-from .multi_line_string import MultiLineStringSchema
-from .polygon import PolygonSchema
-from .multi_point import MultiPointSchema
-
 
 class GeometryCollectionSchema(BaseSchema):
-    point_schema = PointSchema
-    multi_point_schema = MultiPointSchema
-    line_string_schema = LineStringSchema
-    multi_line_string_schema = MultiLineStringSchema
-    polygon_schema = PolygonSchema
-    multi_polygon_schema = MultiPolygonSchema
-
     type = Str(
         required=True,
         validate=OneOf(
---
+++ marshmallow_geojson/property.py
@@ -0,0 +1,5 @@
+from ._base import BaseSchema
+
+
+class PropertiesSchema(BaseSchema):
+    pass

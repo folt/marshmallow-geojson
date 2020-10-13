af2d15ecfbcd93abc036eabcee95eeb59634d92c
folt <foltfrend@list.ru>
2020-10-13 11:34:00 +0300

    init support Feature Schema #19

--- marshmallow_geojson/feature.py
+++ marshmallow_geojson/feature.py
@@ -1,5 +1,5 @@
-import marshmallow as ma
 from marshmallow.fields import (
+    Nested,
     Str,
 )
 from marshmallow.validate import (
@@ -8,12 +8,26 @@ from marshmallow.validate import (
 from .object_type import (
     FEATURE,
 )
+from ._base import BaseSchema
+from .geometry import GeometriesSchema
+from .property import PropertiesSchema
 
 
-class FeatureSchema(ma.Schema):
+class FeatureSchema(BaseSchema):
     type = Str(
         required=True,
         validate=OneOf(
             [FEATURE],
-            error='Invalid feature type'),
+            error='Invalid feature type'
+        )
+    )
+
+    geometry = Nested(
+        GeometriesSchema,
+        required=True,
+    )
+
+    properties = Nested(
+        PropertiesSchema,
+        required=True,
     )
